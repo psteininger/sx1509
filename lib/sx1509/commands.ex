1b6051ee0904dc7977270db9aa8b6e8711fb211b
Piotr Steininger <piotr.steininger@gmail.com>
2021-01-31 20:56:48 +0100

    Added more commands to set values to IO control registers

--- lib/sx1509/commands.ex
+++ lib/sx1509/commands.ex
@@ -48,6 +48,31 @@ defmodule SX1509.Commands do
          do: {:ok, %{device | conn: conn, dir: <<bytes::16>>}}
   end
 
+
+
+  def pin_direction(%{conn: conn, dir: dir} = device, pin, :in) do
+    with bytes <- set_pin(dir, pin),
+         {:ok, conn} <- Registers.IO.write_dir(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, dir: <<bytes::16>>}}
+  end
+
+  @doc """
+  Configures the specified pin's polarity.
+  """
+  @spec pin_polarity(Conn.t(), SX1509.pin_number(), pin_polarity()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def pin_polarity(%{conn: conn, polarity: polarity} = device, pin, :normal) do
+    with bytes <- clear_pin(polarity, pin),
+         {:ok, conn} <- Registers.IO.write_polarity(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, polarity: <<bytes::16>>}}
+  end
+
+  def pin_polarity(%{conn: conn, polarity: polarity} = device, pin, :inverted) do
+    with bytes <- set_pin(polarity, pin),
+         {:ok, conn} <- Registers.IO.write_polarity(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, polarity: <<bytes::16>>}}
+  end
+
   @doc """
   Sets the data value of the pin.
   """
@@ -79,7 +104,7 @@ defmodule SX1509.Commands do
   def disable_input(%{conn: conn, input_disable: input_disable} = device, pin) do
     with bytes <- set_pin(input_disable, pin),
          {:ok, conn} <- Registers.IO.write_input_disable(conn, <<bytes::16>>),
-         do: {:ok, %{device | conn: conn, dir: <<bytes::16>>}}
+         do: {:ok, %{device | conn: conn, input_disable: <<bytes::16>>}}
   end
 
   @doc """
@@ -90,10 +115,74 @@ defmodule SX1509.Commands do
   def enable_input(%{conn: conn, input_disable: input_disable} = device, pin) do
     with bytes <- clear_pin(input_disable, pin),
          {:ok, conn} <- Registers.IO.write_input_disable(conn, <<bytes::16>>),
-         do: {:ok, %{device | conn: conn, dir: <<bytes::16>>}}
+         do: {:ok, %{device | conn: conn, input_disable: <<bytes::16>>}}
   end
 
+  @doc """
+  Enables pull up resistor for specified pin number.
+  """
+  @spec enable_pull_up(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def enable_pull_up(%{conn: conn, pull_up: pull_up} = device, pin) do
+    with bytes <- set_pin(pull_up, pin),
+         {:ok, conn} <- Registers.IO.write_pull_up(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, pull_up: <<bytes::16>>}}
+  end
 
+  @doc """
+  Disables pull up resistor for specified pin number.
+  """
+  @spec disable_pull_up(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def disable_pull_up(%{conn: conn, pull_up: pull_up} = device, pin) do
+    with bytes <- clear_pin(pull_up, pin),
+         {:ok, conn} <- Registers.IO.write_pull_up(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, pull_up: <<bytes::16>>}}
+  end
+
+  @doc """
+  Enables pull down resistor for specified pin number.
+  """
+  @spec enable_pull_down(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def enable_pull_down(%{conn: conn, pull_down: pull_down} = device, pin) do
+    with bytes <- set_pin(pull_down, pin),
+         {:ok, conn} <- Registers.IO.write_pull_down(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, pull_down: <<bytes::16>>}}
+  end
+
+  @doc """
+  Disables pull down resistor for specified pin number.
+  """
+  @spec disable_pull_down(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def disable_pull_down(%{conn: conn, pull_down: pull_down} = device, pin) do
+    with bytes <- clear_pin(pull_down, pin),
+         {:ok, conn} <- Registers.IO.write_pull_down(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, pull_down: <<bytes::16>>}}
+  end
+
+  @doc """
+  Enables open drain operation for specified pin number.
+  """
+  @spec enable_open_drain(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def enable_open_drain(%{conn: conn, open_drain: open_drain} = device, pin) do
+    with bytes <- set_pin(open_drain, pin),
+         {:ok, conn} <- Registers.IO.write_open_drain(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, open_drain: <<bytes::16>>}}
+  end
+
+  @doc """
+  Enables open drain operation for specified pin number.
+  """
+  @spec disable_open_drain(Conn.t(), SX1509.pin_number()) ::
+          {:ok, Conn.t()} | {:error, reason :: any}
+  def disable_open_drain(%{conn: conn, open_drain: open_drain} = device, pin) do
+    with bytes <- clear_pin(open_drain, pin),
+         {:ok, conn} <- Registers.IO.write_open_drain(conn, <<bytes::16>>),
+         do: {:ok, %{device | conn: conn, open_drain: <<bytes::16>>}}
+  end
 
   defp clear_pin(<<bytes::16>>, pin) when is_pin_number(pin) do
     bytes &&& ~~~ (1 <<< pin)
